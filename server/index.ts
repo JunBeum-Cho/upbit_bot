8481b24abae1cc54d9d043d028ab0fb60e14325c
JunBeum-Cho <junbeumcho@gmail.com>
2021-01-12 18:44:45 +0900

    test 중

--- server/index.ts
+++ server/index.ts
@@ -27,10 +27,13 @@ let upbit_process = cp.spawn('npx', ['ts-node','upbit.ts'], {stdio: 'inherit'})
 redis_storage.set("status", "running")
 
 app.get("/", function(req, res) {
+  console.log(__dirname)
+    console.log(path.join(__dirname, "..", "client","build"))
     res.sendFile("/index.html")
 })
 
 app.get("/bot", function(req, res) {
+  console.log(path.join(__dirname, "..", "client","build"))
   res.sendFile("/index.html")
 })
 
